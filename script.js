4435497dc31bdffcc83681c9e36ed2db1edb2b52
schazbot <charlotte.pearce1984@gmail.com>
2020-01-21 10:56:48 +0000

    adding route line onto map, but layering on top each time

--- script.js
+++ script.js
@@ -25,22 +25,22 @@ L.tileLayer(
 ).addTo(mymap);
 
 //functions
+const renderMapFromPolylineString = polylineString => {
+  //     debugger
+    var encodedRoutes = [polylineString];
 
-var encodedRoutes = [
-  "um_yHdnFa@kAi@eCBgARgB?Q]m@iAo@kCcCoH{FoCgB{@E{FXYVcAjBk@bBmCnIq@lDEBmFoDsDaDeAaBiBuBmAw@oB_@oCHaBXiAf@sBdCkA|@aQnFeB\\q@S_@[u@c@m@UcDy@oBKuAFmAj@yBT{@EkCw@{F}@{Ac@cC}CeEmIUUyGvI_CjD{@vBi@jC?dBR|BpEbZE\\SRaAJiD~@OT@fAIZ}EjB}BTkBfAkBlBgAhBaAlCOLgDe@QJQfB_AjDA|@TxBAd@aAvF_@f@O@c@[gA{AgCkAqETiCKGBY|@M@}Bg@gB{@YN}AbBwBxDeBzEsAdGqBpKBnBm@vN[nAd@fFOjDSpBl@`IB`@KP{Bz@s@DSt@ODuMg@Wb@a@`IE^SP_HqAo@T{DrCcDNoGn@qBdAKXUxD_AbGm@|Bg@nAKp@y@|C{DrIqBdIsB`Gy@nBoFpJqBrF[vAk@zDqDfJYfBa@~AmDtLs@xC?ZKpAm@rDSjCOHsAi@gM}CoAg@Ka@g@CuBuAmAYe@l@kBrAqDjDiCtA[|@[b@iC~@mCbBc@By@YoCsAeHcCq@[UWoBOaB}@}@_A_@Lo@hBKtAa@bA]Ng@sCk@_AnGbCKFy@FLu@QQDkAHe@TQPAiAzBrDlAtCj@j@v@?R`Bb@zFF`@HZW`EyA~BMj@P^gAd@i@fGwBnAgAXa@CKJUZ[xBIjBx@dH|@`NnCvAfAfAh@t@|@dFjJ|AvArAtBhAnAtC`Ah@UbA}@l@_AnCoA`A[dBqA|FuGx@o@z@a@dC_Dv@_B~@qArByBzCmC`@iAn@eDP[j@Wx@Pv@c@fGwHxAqAdDwEpAsAxDaHxDqIvCgInBuDlBcFn@kArAIhIRzDUpADHIVeCR]b@WXGnDf@`@IlBs@p@c@~FeBpBiAdDq@nFqA`Ac@pAcAvDa@dDo@r@g@bBg@\\Bh@QdEGd@B^XlBK`Bw@tC_CbHeErCuAtBcBjAaBfAiA\\MvDLrBKfHeDhCiB^Dl@k@t@aARm@pD{G~@}BdAgBfAqDl@iCrBwFjBkI~AsFn@yF@{@QgFEuESoDF]nDgFDU{BwGGc@`@_AnDgF`@u@nBwInBkEtAcCbB}DZuAz@_GPu@TS^NfGzDd@HXs@t@iGJKd@@PUxCq@dAIxBHrB~@~@t@Zl@|@r@`@E|BkACk@YeBGEYR"
-];
+  for (let encoded of encodedRoutes) {
+    var coordinates = L.Polyline.fromEncoded(encoded).getLatLngs();
 
-for (let encoded of encodedRoutes) {
-  var coordinates = L.Polyline.fromEncoded(encoded).getLatLngs();
-
-  let polyline = L.polyline(coordinates, {
-    color: "blue",
-    weight: 2,
-    opacity: 0.7,
-    lineJoin: "round"
-  }).addTo(mymap);
-  mymap.fitBounds(polyline.getBounds());
-}
+    let polyline = L.polyline(coordinates, {
+      color: "blue",
+      weight: 2,
+      opacity: 0.7,
+      lineJoin: "round"
+    }).addTo(mymap);
+    mymap.fitBounds(polyline.getBounds());
+  }
+};
 
 const renderActivityCard = activities => {
   d3.select(".chart")
@@ -52,7 +52,7 @@ const renderActivityCard = activities => {
     .transition()
     .duration(2000)
     .style("width", function(d) {
-      return (d.distance/1000) * 10 + "px";
+      return (d.distance / 1000) * 10 + "px";
     })
     .style("background-color", function(d) {
       if (d.distance / 1000 <= 20) {
@@ -63,13 +63,14 @@ const renderActivityCard = activities => {
         return "darkcyan";
       }
     })
-      .text(function (d) {
-        let formattedDistance = (d.distance/1000).toFixed(1)
+    .text(function(d) {
+      let formattedDistance = (d.distance / 1000).toFixed(1);
       return `${formattedDistance}km`;
     });
 
-  d3.selectAll("div").on("click", function(d, i) {
-    console.log(d);
+  d3.selectAll("div").on("click", d => {
+    // debugger;
+    renderMapFromPolylineString(d.map.summary_polyline);
   });
 };
 
